54c502ba5496407e585d817fab08fb286dcb5240
plaidPillagers <lscott@plaidpillagers.com>
2017-01-28 15:42:16 -0600

    got encoder up

--- Steamworks/src/org/usfirst/frc2538/Steamworks/commands/DriveCommand.java
+++ Steamworks/src/org/usfirst/frc2538/Steamworks/commands/DriveCommand.java
@@ -42,7 +42,7 @@ public class DriveCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.drive();
+    	Robot.drivetrain.drive2();
     }
 
     // Make this return true when this Command no longer needs to run execute()
